176c6781b0f907f10bd106e78d02ae744632d88d
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2017-01-18 08:12:22 -0500

    reformat code

--- core/models.py
+++ core/models.py
@@ -4,66 +4,84 @@ ESCALAS = (
     ('1', 'Totalmente en desacuerdo'),
     ('2', 'En desacuerdo'),
     ('3', 'Ni acuerdo ni desacuerdo'),
-    ('4','De acuerdo'),
-    ('5','Totalmente de acuerdo'),
+    ('4', 'De acuerdo'),
+    ('5', 'Totalmente de acuerdo'),
 )
-# Create your models here.
+
+
 class Persona(models.Model):
-    codigo = models.CharField(max_length=6,unique=True)
+    codigo = models.CharField(max_length=6, unique=True)
     email = models.EmailField()
+
     def __str__(self):
         return self.codigo
 
 
 class Encuesta(models.Model):
     descripcion = models.CharField(max_length=128)
+
     def __str__(self):
         return self.descripcion
 
+
 class Pregunta(models.Model):
     enunciado = models.CharField(max_length=128)
-    pregunta = models.ForeignKey(Encuesta,related_name='preguntas')
+    pregunta = models.ForeignKey(Encuesta, related_name='preguntas')
+
     def __str__(self):
         return self.enunciado
+
 
 class Periodo(models.Model):
     descripcion = models.CharField(max_length=128)
     fechaIncio = models.DateField()
     fechaFin = models.DateField()
+
     def __str__(self):
         return self.descripcion
+
 
 class Registro(models.Model):
     pregunta = models.ForeignKey(Pregunta)
     codigo = models.ForeignKey(Persona)
     periodo = models.ForeignKey(Periodo)
     escala = models.CharField(max_length=128, choices=ESCALAS)
+
     def __str__(self):
         return self.escala
+
     class Meta:
-        unique_together=('pregunta','codigo','periodo',)
+        unique_together = ('pregunta', 'codigo', 'periodo',)
+
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=128)
+
     def __str__(self):
         return self.nombre
+
 
 class Libro(models.Model):
     libro = models.CharField(max_length=8)
     nombre = models.CharField(max_length=128)
     categoria = models.ForeignKey(Categoria)
+
     def __str__(self):
         return self.libro
+
 
 class Busqueda(models.Model):
     libro = models.ForeignKey(Libro)
     periodo = models.ForeignKey(Periodo)
+
     def __str__(self):
-        return '%s'%self.libro
+        return '%s' % self.libro
+
 
 class Prestamo(models.Model):
     libro = models.ForeignKey(Libro)
     periodo = models.ForeignKey(Periodo)
     persona = models.ForeignKey(Persona)
+
     def __str__(self):
-        return '%s, %s, %s'%(self.libro,self.persona, self.periodo,)
+        return '%s, %s, %s' % (self.libro, self.persona, self.periodo,)
